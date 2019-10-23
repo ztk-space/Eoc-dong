32bec1a178983cccf39e3b4d5e0be9d29a87971b
ztk-space <13520612491@163.com>
2019-10-23 16:11:08 +0800

    注释

--- app/src/main/java/com/rk/mynewdome/MainActivity.java
+++ app/src/main/java/com/rk/mynewdome/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import com.donkingliang.imageselector.utils.ImageSelector;
 import com.donkingliang.imageselector.utils.ImageSelectorUtils;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -24,23 +29,25 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recy);
 
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ImageSelectorUtils.openPhoto(MainActivity.this, REQUEST_CODE, false, 9);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        ImageSelectorUtils.openPhoto(MainActivity.this, REQUEST_CODE, false, 9);
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && data != null) {
-            //获取选择器返回的数据
-            ArrayList<String> images = data.getStringArrayListExtra(
-                    ImageSelectorUtils.SELECT_RESULT);
-            recyclerView.setAdapter(new RecyAdper(images,this));
-            for (int i = 0;i<images.size();i++){
-                String s = images.get(i);
-                s.replace("jpg","PNG");
-                Log.i("TAG",s);
-            }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_CODE && data != null) {
+//            //获取选择器返回的数据
+//            ArrayList<String> images = data.getStringArrayListExtra(
+//                    ImageSelectorUtils.SELECT_RESULT);
+//            recyclerView.setAdapter(new RecyAdper(images,this));
+//            for (int i = 0;i<images.size();i++){
+//                String s = images.get(i);
+//                s.replace("jpg","PNG");
+//                Log.i("TAG",s);
+//            }
+//
+//        }
+//    }
 
-        }
-    }
+
 }
